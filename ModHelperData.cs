10920d84d8530b5e4bc11f43ae6eff5c312efc2f
Ross Watts <ross.n.watts@gmail.com>
2024-06-23 21:13:12 +0100

    Updated mod description.

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Impoppable Plus";
 
-    public const string Description = "Extends Impoppable mode by making it an entirely new difficulty, complete with 3 new game modes: "
-                                    + "Support Only, Double Speed and Mastery Mode.";
+    public const string Description = "Extends Impoppable mode by making it an entirely new difficulty, complete with 4 new game modes: "
+                                    + "Support Only, Double Speed, Mastery Mode and Impoppable Sandbox";
     public const string RepoOwner = "Ross-TheBoss"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "ImpoppablePlus"; // TODO add your repo name here, also in the download url in README.md
     public const string Author = "Timotheee and Ross-TheBoss";
