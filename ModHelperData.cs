5a232971a4535b9e841d2ae513254497dfa9b00c
Ross-TheBoss <ross.n.watts@gmail.com>
2026-04-15 11:32:38 +0100

    Updated git workflow and WorksOnVersion.

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace ImpoppablePlus;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "42.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "47.0";
+    public const string Version = "1.0.1";
     public const string Name = "Impoppable Plus";
 
     public const string Description = "Extends Impoppable mode by making it an entirely new difficulty, complete with 4 new game modes: "
